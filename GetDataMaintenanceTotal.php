6101bb03a26dde034daf29748980c04349449371
Daffa Abiyyu <daffaabiyyu04@gmail.com>
2024-07-23 01:38:43 +0700

    teuing

--- GetDataMaintenanceTotal.php
+++ GetDataMaintenanceTotal.php
@@ -3,14 +3,14 @@
 require_once('koneksi.php');
 
 // Query untuk menghitung total pbk_id dengan pbk_jenis 2
-$query = "SELECT COUNT(pbk_id) as total FROM `mmo_perbaikan` WHERE pbk_jenis = 2";
+$query = "SELECT COUNT(pbk_id) as total FROM mmo_perbaikan WHERE pbk_jenis = 2";
 $select = mysqli_query($conn, $query);
 
 $result = array();
 if ($row = mysqli_fetch_array($select)) {
-    $result['total'] = $row['total'];
+    $result['result'] = $row['total'];
 } else {
-    $result['total'] = 0;
+    $result['result'] = 0;
 }
 
 echo json_encode($result);
